9e49dacdb9b739381efa8f2d2b151f3ba1f8a435
Bigardcode <84800557+Bigardcode@users.noreply.github.com>
2023-01-11 21:04:10 +0330

    Update Mutation_circular_plot.R

--- Mutation_circular_plot.R
+++ Mutation_circular_plot.R
@@ -1,31 +1,19 @@
 
-
 library(ggplot2)
 library(plotrix)
 library(circlize)
 library(pacman)
 
-
-
-
-
-
-
-tiff('Mutation_Circular_plot1.tiff', units="in", width=5, height=3.5, res=600, 
-     compression = 'lzw')
+tiff('Mutation_Circular_plot1.tiff', units="in", width=5, height=3.5, res=600, compression = 'lzw')
 
 
 category <- c("HGNC", "ADCY5", "APOC3", "BCL2","CDKN1A", "CTF1", "EGR1", "FEN1", "GDF11")
 
 percent <- c(75, 65, 45, 69, 35, 44, 62, 80, 50)
 
-color= c("#7C0A02", "#FAE7B5", "#7C0A02", "#FAE7B5", 
-         "#7C0A02", "#FAE7B5", "#7C0A02", "#FAE7B5", "#7C0A02")
+color= c("#7C0A02", "#FAE7B5", "#7C0A02", "#FAE7B5", "#7C0A02", "#FAE7B5", "#7C0A02", "#FAE7B5", "#7C0A02")
 
 
-
-
-library(circlize)
 circos.par("start.degree" = 90, cell.padding = c(0, 0, 0, 0))
 circos.initialize("a", xlim = c(0, 100)) # 'a` just means there is one sector
 circos.track(ylim = c(0.5, length(percent)+0.5), track.height = 0.8, 
